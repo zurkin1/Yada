732d5bdbb3fb4af13eb807f67f803de394e6bed2
Dani Livne <33825810+zurkin1@users.noreply.github.com>
2020-10-12 20:15:23 +0300

    Update yada.py

--- yada.py
+++ yada.py
@@ -31,17 +31,17 @@ def run_deconv(mix, methods):
         print(f'{method[1].__code__.co_name}, {method[3]}')
         pure = pd.read_csv('./data/' + method[3] + '.csv', index_col=0)
         # Drop genes that are not shared by mix and pure.
-        both_genes = list(set(mix.index) & set(pure.index))  # - set(BRCA)
-        pure = pure.reindex(both_genes)
+        #both_genes = list(set(mix.index) & set(pure.index))  # - set(BRCA)
+        #pure = pure.reindex(both_genes)
         mix_loop = mix.copy()
-        mix_loop = mix_loop.reindex(both_genes)
-        gene_list_df = gene_diff(pure, mix_loop)
+        #mix_loop = mix_loop.reindex(both_genes)
+        #gene_list_df = gene_diff(pure, mix_loop)
 
         num_cells = len(pure.columns)
         ens_estimate_wt = np.zeros((num_cells, num_mixes))
         estimate_wt = np.zeros((num_cells, num_mixes))
 
-        results = [pool.apply_async(method[1], args=(mix_loop, pure, gene_list_df)) for i in range(method[0])]
+        results = [pool.apply_async(method[1], args=(mix_loop, pure, pure)) for i in range(method[0])]
         for ens_i in range(method[0]):
             print('\r', f"{ens_i / method[0] * 100:.0f}%", end='')
             ens_estimate_wt += results[ens_i].get()  # estimate_wt
