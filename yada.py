3bc74e138edb6680b456cf8c5bd432fb265477f2
Dani Livne <33825810+zurkin1@users.noreply.github.com>
2020-12-20 23:54:07 +0200

    Update yada.py

--- yada.py
+++ yada.py
@@ -22,36 +22,35 @@ def calc_corr(test, ens_estimate_wt_2):
             print(f'{test["dataset.name"]}, {col}, {np.corrcoef(real_weight[col], ens_estimate_wt_2[col])[0][1]}, {st.spearmanr(real_weight[col], ens_estimate_wt_2[col])}')
 
 
-def run_deconv(mix, methods):
+def run_deconv(num, mix, pure, method):
     pool = mp.Pool()
     mix = pd.read_csv(mix, index_col=0)
     mix = mix.groupby(mix.index).first()
     mix.index = mix.index.str.lower()
     num_mixes = len(mix.columns)
     print(f'Deconvolution, num_mixes: {num_mixes}')
-    for method in methods:
-        print(f'{method[1].__code__.co_name}, {method[3]}')
-        pure = pd.read_csv(method[3], index_col=0)
-        gene_list_df = pure
+    print(f'{method.__code__.co_name}, {method[3]}')
+    pure = pd.read_csv(pure, index_col=0)
+    gene_list_df = pure
 
-        # Gene differentiation algorithm.
-        # Drop genes that are not shared by mix and pure.
-        both_genes = list(set(mix.index) & set(pure.index))  # - set(BRCA)
-        pure = pure.reindex(both_genes)
-        mix_loop = mix.copy()
-        mix_loop = mix_loop.reindex(both_genes)
-        gene_list_df = gene_diff(pure, mix_loop)
+    # Gene differentiation algorithm.
+    # Drop genes that are not shared by mix and pure.
+    both_genes = list(set(mix.index) & set(pure.index))  # - set(BRCA)
+    pure = pure.reindex(both_genes)
+    mix_loop = mix.copy()
+    mix_loop = mix_loop.reindex(both_genes)
+    gene_list_df = gene_diff(pure, mix_loop)
 
-        num_cells = len(pure.columns)
-        ens_estimate_wt = np.zeros((num_cells, num_mixes))
-        estimate_wt = np.zeros((num_cells, num_mixes))
+    num_cells = len(pure.columns)
+    ens_estimate_wt = np.zeros((num_cells, num_mixes))
+    estimate_wt = np.zeros((num_cells, num_mixes))
 
-        results = [pool.apply_async(method[1], args=(mix_loop, pure, gene_list_df)) for i in range(method[0])]
-        for ens_i in range(method[0]):
-            print('\r', f"{ens_i / method[0] * 100:.0f}%", end='')
-            ens_estimate_wt += results[ens_i].get()  # estimate_wt
-        ens_estimate_wt /= method[0]
-        ens_estimate_wt = pd.DataFrame(data=ens_estimate_wt, index=pure.columns).T
-        ens_estimate_wt.to_csv('./data/results.csv')
-        return(ens_estimate_wt[method[2]])
+    results = [pool.apply_async(method, args=(mix_loop, pure, gene_list_df)) for i in range(num)]
+    for ens_i in range(num):
+        print('\r', f"{ens_i / num * 100:.0f}%", end='')
+        ens_estimate_wt += results[ens_i].get()  # estimate_wt
+    ens_estimate_wt /= num
+    ens_estimate_wt = pd.DataFrame(data=ens_estimate_wt, index=pure.columns).T
+    ens_estimate_wt.to_csv('./data/results.csv')
     pool.close()
+    return(ens_estimate_wt)
