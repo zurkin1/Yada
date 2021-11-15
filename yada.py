b6d9c2107aa71edeac266f2da9271f811695dfba
Dani Livne <33825810+zurkin1@users.noreply.github.com>
2021-11-15 16:16:10 +0200

    Change.

--- yada.py
+++ yada.py
@@ -23,13 +23,15 @@ def calc_corr(test, ens_estimate_wt_2):
 
 
 def run_dtw_deconv(mix, pure):
-    num_loops = 100
-    pool = mp.Pool()
     mix = pd.read_csv(mix, index_col=0)
-    mix = mix.groupby(mix.index).first()
     mix.index = mix.index.map(str.lower)
+    mix.index = mix.index.map(str.strip)
+    mix = mix.groupby(mix.index).first()
     num_mixes = len(mix.columns)
     pure = pd.read_csv(pure, index_col=0)
+    pure.index = pure.index.map(str.lower)
+    pure.index = pure.index.map(str.strip)
+    pure = pure.groupby(pure.index).first()
     print(f'Deconvolution, num_cells: {len(pure.columns)}, num_mixes: {num_mixes}')
 
      # Drop genes that are not shared by mix and pure.
@@ -44,12 +46,15 @@ def run_dtw_deconv(mix, pure):
     ens_estimate_wt = np.zeros((num_cells, num_mixes))
     estimate_wt = np.zeros((num_cells, num_mixes))
 
-    results = [pool.apply_async(dtw_deconv, args=(mix, pure, gene_list_df)) for i in range(num_loops)]
+    num_loops = 100
+    #pool = mp.Pool()
+    #results = [pool.apply_async(dtw_deconv, args=(mix, pure, gene_list_df)) for i in range(num_loops)]
+    results = [dtw_deconv(mix, pure, gene_list_df) for i in range(num_loops)]
     for ens_i in range(num_loops):
         print('\r', f"{ens_i / num_loops * 100:.0f}%", end='')
-        ens_estimate_wt += results[ens_i].get()  # estimate_wt
+        ens_estimate_wt += results[ens_i] #.get()  # estimate_wt
     ens_estimate_wt /= num_loops
     ens_estimate_wt = pd.DataFrame(data=ens_estimate_wt, index=pure.columns).T
     ens_estimate_wt.to_csv('./data/results.csv')
-    pool.close()
+    #pool.close()
     return(ens_estimate_wt)
