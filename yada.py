f4cb0f3bc7f47a797f684c4273899bd69a74e23c
Dani Livne <33825810+zurkin1@users.noreply.github.com>
2021-12-19 22:36:21 +0200

    Change.

--- yada.py
+++ yada.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from diffexp import gene_diff
-from func import *
+from Yada.diffexp import gene_diff
+from Yada.func import *
 # import matplotlib
 # import matplotlib.pyplot as plt
 import scipy.stats as st
@@ -9,9 +9,12 @@ import multiprocessing as mp
 import time
 
 
+relative_path = './Yada/'
+
+
 def calc_corr(metric, prop, platform, ens_estimate_wt_2):
     #real_weight = pd.read_csv('./data/Challenge/prop-' + prop, index_col=0).T
-    real_weight = pd.read_csv(f'./data/{prop}/labels.csv', index_col=0)
+    real_weight = pd.read_csv(relative_path + f'./data/{prop}/labels.csv', index_col=0)
     # matplotlib.style.use('ggplot')
     result = []
     for col in real_weight:
@@ -34,20 +37,20 @@ def run_deconv(mix, pure, test, metric):
     mix.index = mix.index.map(str.lower)
     mix.index = mix.index.map(str.strip)
     mix = mix.groupby(mix.index).first()
-    #if mix.max().max() < 20:
-    #    mix = 2 ** mix
+    if mix.max().max() < 20:
+        mix = 2 ** mix
     num_mixes = len(mix.columns)
     pure = pd.read_csv(pure, index_col=0)
     pure.index = pure.index.map(str.lower)
     pure.index = pure.index.map(str.strip)
     pure = pure.groupby(pure.index).first()
-    #if pure.max().max() < 20:
-    #    pure = 2 ** pure
+    if pure.max().max() < 20:
+        pure = 2 ** pure
 
      # Drop genes that are not shared by mix and pure.
     both_genes = list(set(mix.index) & set(pure.index))  # - set(BRCA)
-    pure = pure.reindex(both_genes)
-    mix = mix.reindex(both_genes)
+    #pure = pure.reindex(both_genes)
+    #mix = mix.reindex(both_genes)
 
     # Gene differentiation algorithm.
     gene_list_df = gene_diff(pure, mix)
@@ -56,11 +59,11 @@ def run_deconv(mix, pure, test, metric):
     num_cells = len(pure.columns)
     ens_estimate_wt = np.zeros((num_cells, num_mixes))
     
-    results = [pool.apply_async(dtw_deconv, args=(mix, pure, gene_list_df, metric)) for i in range(num_loops)]
-    #results = [dtw_deconv(mix, pure, gene_list_df) for i in range(num_loops)]
+    #results = [pool.apply_async(dtw_deconv, args=(mix, pure, gene_list_df, metric)) for i in range(num_loops)]
+    results = [dtw_deconv(mix, pure, gene_list_df, metric) for i in range(num_loops)]
     for ens_i in range(num_loops):
         print('\r', f"{ens_i / num_loops * 100:.0f}%", end='')
-        ens_estimate_wt += results[ens_i].get()
+        ens_estimate_wt += results[ens_i] #.get()
     ens_estimate_wt /= num_loops
     ens_estimate_wt = pd.DataFrame(data=ens_estimate_wt, index=pure.columns).T
     #ens_estimate_wt.to_csv('./data/results.csv')
@@ -87,11 +90,11 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     result = pd.DataFrame([['a', 'b', 'c', 'd', 0.0, 0.0, 0.0]]*2)
-    for metric in ['dtw', 'avg', 'abs', 'basic', 'ks', 'euclid', 'taxi']:
-        for file in ['10x', 'Abbas', 'BreastBlood', 'xCell', 'CIBERSORT', 'DeconRNASeq', 'DSA', 'EPIC', 'RatBrain', 'TIMER']:
+    for metric in ['pathweigh']: #'dtw', 'avg', 'abs', 'basic', 'ks', 'euclid', 'taxi']:
+        for file in ['Challenge']: #'10x', 'Abbas', 'BreastBlood', CIBERSORT', 'DeconRNASeq', 'DSA', 'EPIC', 'xCell', 'RatBrain', 'TIMER']:
             print(metric, file, ' ', end="")
-            res = run_deconv(f'./data/{file}/mix.csv', f'./data/{file}/pure.csv', file, metric)
+            res = run_deconv(relative_path + f'./data/{file}/mix.csv', relative_path + f'./data/{file}/pure.csv', file, metric)
             file_res = pd.DataFrame(calc_corr(metric, file, 'Microarray', res))
             result = pd.concat([result, file_res])
         print(result.loc[result[0] == metric].describe())
-    result[2:].to_csv('./data/result.csv')
+    result[2:].to_csv(relative_path + './data/result.csv')
